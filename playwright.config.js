94b8997e79889e26581efa7bbc65a89834327a6a
Sayan <sayanbhandari007@gmail.com>
2026-05-20 23:09:28 +0530

    parallel:false

--- playwright.config.js
+++ playwright.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
   testDir: './tests',
   timeout: 60000,
   // Use Playwright defaults for test and expect timeouts; avoid overriding here
-  fullyParallel: true,
-  workers: 5,
+  fullyParallel: false,
+  workers: 1,
   retries: 1,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
