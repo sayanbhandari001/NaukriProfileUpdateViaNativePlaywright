a6ec1bb6ee6b0999b1edc7341e4047f0cf886fb4
Sayan Bhandari <sayanbhandari007@gmail.com>
2026-06-01 20:39:40 +0530

    latest all commit for international websites

--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Test group', () => {
-  test('seed', async ({ page }) => {
+  test.skip('seed', async ({ page }) => {
     // generate code here.
   });
 });
